d97e7471681bad6ccb523b63460b8c458e7cef8f
Romalyy <1303roma01@gmail.com>
2022-10-01 19:14:42 +0300

    auth

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -3,30 +3,33 @@ const router = express.Router();
 
 const ctrl = require('../../controllers/contacts');
 
-const { validateContactBody, isValidId } = require('../../middlewares');
+const { validateContactBody, isValidId, authenticate } = require('../../middlewares');
 
 const { schemas } = require('../../models/contact');
 
-router.get('/', ctrl.getContactsList);
+router.get('/', authenticate, ctrl.getContactsList);
 
-router.get('/:id', isValidId, ctrl.getContactById);
+router.get('/:id', authenticate, isValidId, ctrl.getContactById);
 
 router.post(
   '/',
+  authenticate,
   validateContactBody(schemas.contactAddSchema),
   ctrl.addContact
 );
 
-router.delete('/:id', isValidId, ctrl.removeById);
+router.delete('/:id', authenticate, isValidId, ctrl.removeById);
 
 router.put(
   '/:id',
+  authenticate,
   isValidId,
   validateContactBody(schemas.contactAddSchema),
   ctrl.updateById
 );
 router.patch(
   '/:id/favorite',
+  authenticate,
   isValidId,
   validateContactBody(schemas.updFavorContactSchema),
   ctrl.updFavorContact
